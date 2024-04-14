8f403f04267b94e413acf82e5ff911128e965b1d
forgedreality <forgedreality@gmail.com>
2024-04-13 21:26:13 -0700

    Add files via upload

--- Code50/hello/hello.c
+++ Code50/hello/hello.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void)
+{
+    // get a name input from user
+    string name = get_string("What is your name? ");
+    printf("hello, %s\n", name);
+}
