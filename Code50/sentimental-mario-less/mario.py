e63939b7790b4ac118fc23e6dede068cf688c26e
forgedreality <forgedreality@gmail.com>
2024-04-13 21:49:55 -0700

    Add files via upload

--- Code50/sentimental-mario-less/mario.py
+++ Code50/sentimental-mario-less/mario.py
@@ -1,1 +1,19 @@
+# Validate input
+while True:
+    try:
+        h = int(input('Height: '))
+        if h < 1 or h > 8:
+            raise ValueError
+        break
+    except ValueError:
+        print("Invalid input.  Please enter a number between 1 and 8.")
 
+# Iterate
+for i in range(h):
+    # Calc spaces
+    n = (h - 1) - i
+    print(" " * n, end="")
+
+    # Calc hashes
+    m = h - n
+    print("#" * m)
